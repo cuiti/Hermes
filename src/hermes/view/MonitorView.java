939e192c5f0384874591a90905b4278b69181938
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2015-10-27 11:06:48 -0300

    algo básico de la vista

--- src/hermes/view/MonitorView.java
+++ src/hermes/view/MonitorView.java
@@ -1,14 +1,28 @@
 package hermes.view;
 
-public class MonitorView {
+import java.awt.*;
+import javax.swing.*;
 
+public class MonitorView extends JFrame{
+
+	private JPanel panelPrincipal;
+	
 	public MonitorView() {
+		panelPrincipal = new JPanel();
+		getContentPane().add(panelPrincipal);
+		panelPrincipal.setLayout(new BorderLayout());
 		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Hermes | Monitor para terapeuta");	
+		setSize(880, 500);
+		//setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MonitorView view = new MonitorView();
 	}
 
 }
