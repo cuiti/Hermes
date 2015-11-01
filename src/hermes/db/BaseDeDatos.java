4fa3e1f41887cd883061d0e43b115b9da17ce93d
Alexander <r11_alex@hotmail.com>
2015-11-01 20:03:19 -0300

    Quitada ruta absoluta de bd

--- src/hermes/db/BaseDeDatos.java
+++ src/hermes/db/BaseDeDatos.java
@@ -9,14 +9,14 @@ public class BaseDeDatos {
 	
 	public Connection getConnection(){
 		
-		Connection c= null;
+		Connection c = null;
 		try {
 		      Class.forName("org.sqlite.JDBC");
-		      c = DriverManager.getConnection("jdbc:sqlite:C:/dev/workspace/Hermes-desktop/hermes.sqlite");
+		      c = DriverManager.getConnection("jdbc:sqlite:"+System.getProperty("user.dir")+"/hermes.sqlite");
 		      System.out.println("Opened database successfully");
-		 } catch ( Exception e ) {
+		} catch ( Exception e ) {
 		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		 }
+		}
 		 
 		return c;
 		
