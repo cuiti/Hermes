30d18558fe15c33bb7007464ddb4ee855b7d8090
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2015-11-25 15:44:19 -0300

    Cambio por Gson en Testeador

--- src/hermes/anotaciones/Testeador.java
+++ src/hermes/anotaciones/Testeador.java
@@ -2,13 +2,22 @@ package hermes.anotaciones;
 
 import java.util.List;
 
+import com.google.gson.*;
+
 public class Testeador {
 
 	public static void main(String[] args) {
+		
+		Gson gson = new GsonBuilder().setDateFormat("dd/MM/yyyy HH:mm").create();
+		
 		try {
 			List<NotificacionDTO> lista = MockGenerator.createMockInstances(NotificacionDTO.class, 10);
 			for (NotificacionDTO n: lista) {
 				System.out.println(n.getNombre()+" "+n.getApellido()+" "+n.getFecha_envio());
+				
+				String notiJson = gson.toJson(n);
+				System.out.println(notiJson);
+				//enviar notiJson
 			}
 		} catch (Exception e) {
 			System.out.println("Error en el Testeador");
