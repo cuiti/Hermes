307b42f4a91b365c2f348c75c206e5d8d67ce593
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2015-11-25 15:37:06 -0300

    Cambio por Gson en el testeador

--- src/hermes/anotaciones/Testeador.java
+++ src/hermes/anotaciones/Testeador.java
@@ -1,35 +1,17 @@
 package hermes.anotaciones;
 
 import java.util.List;
-import org.json.simple.JSONObject;
 
 public class Testeador {
 
 	public static void main(String[] args) {
-/*		try {
+		try {
 			List<NotificacionDTO> lista = MockGenerator.createMockInstances(NotificacionDTO.class, 10);
 			for (NotificacionDTO n: lista) {
 				System.out.println(n.getNombre()+" "+n.getApellido()+" "+n.getFecha_envio());
 			}
 		} catch (Exception e) {
 			System.out.println("Error en el Testeador");
-			e.printStackTrace();
-		}*/
-		try {
-			List<NotificacionDTO> lista = MockGenerator.createMockInstances(NotificacionDTO.class, 10);
-			for (NotificacionDTO n: lista) {
-				JSONObject jsonNotificacion = new JSONObject();
-				jsonNotificacion.put("fecha_envio", n.getFecha_envio());
-				jsonNotificacion.put("contenido", n.getContenido());
-				jsonNotificacion.put("contexto", n.getContexto());
-				jsonNotificacion.put("categoria", n.getCategoria());
-				jsonNotificacion.put("nombre", n.getNombre());
-				jsonNotificacion.put("apellido", n.getApellido());
-				//System.out.println(jsonNotificacion);
-				//requestMonitor(jsonNotificacion.toJSONString());
-			}
-		} catch (Exception e) {
-			System.out.println("Error en Test");
 			e.printStackTrace();
 		}
 	}
